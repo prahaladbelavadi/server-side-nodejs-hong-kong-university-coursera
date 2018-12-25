3ba5f4d84c6853d90461dbed242530a45c9b0897
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2018-12-26 02:35:24 +0530

    simple node module

---
+++ node-examples/index.js
@@ -0,0 +1,20 @@
+var rect = {
+    perimeter: (x,y) => (2*(x+y)),
+    area: (x,y) => (x*y)
+};
+
+function solveRect(l,b){
+    console.log("Solving for rectangle with l = "+l+" and b= "+b);
+    
+    if (l <= 0 || b<=0){
+        console.log("rectangle length and breadth must be greater than 0")
+    }else{
+        console.log("The area of the rectangle is " + rect.area(l,b));
+        console.log("The permitetr of the rectangle is "+ rect.perimeter(l,b));
+               
+    }
+}
+
+solveRect(1,4);
+solveRect(2,5);
+solveRect(5,-1)
