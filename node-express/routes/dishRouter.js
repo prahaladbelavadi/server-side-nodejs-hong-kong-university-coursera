7f2a69622811c1c2855e1bfbf48433ba5fcbc067
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2018-12-29 13:57:36 +0530

    unable to verify changes between prvious commit and this; last commit showed no body error; I dont know what made this work

--- node-express/routes/dishRouter.js
+++ node-express/routes/dishRouter.js
@@ -1,28 +1,28 @@
-const express =  require('express');
+const express = require('express');
 const bodyParser = require('body-parser');
 
-const dishRouter =  express.Router();
+const dishRouter = express.Router();
 
 dishRouter.use(bodyParser.json());
 
 dishRouter.route('/')
-.all((req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-})
-.get((req, res, next) => {
-    res.end('Will send all the dishes to yuo!');
-})
-.post((req, res, next) => {
-    res.end('Will add the dish: ' + req.body.name + 'with details' + req.body.description);
-})
-.put((req, res, next) => {
-    res.statusCode = 403;
-    res.end('Put operation not supported on /dishes');
-})
-.delete((req, res, next) => {
-    res.end('Deleting all the dishes!');
-});
+    .all((req, res, next) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        next();
+    })
+    .get((req, res, next) => {
+        res.end('Will send all the dishes to you!');
+    })
+    .post((req, res, next) => {
+        res.end('Will add the dish: ' + req.body.name + ' with details: ' + req.body.description);
+    })
+    .put((req, res, next) => {
+        res.statusCode = 403;
+        res.end('PUT operation not supported on /dishes');
+    })
+    .delete((req, res, next) => {
+        res.end('Deleting all dishes');
+    });
 
 module.exports = dishRouter;
