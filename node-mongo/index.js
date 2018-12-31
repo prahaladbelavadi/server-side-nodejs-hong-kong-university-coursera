b8754749b41b5cae9f841927e2a7814455caf4a0
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2018-12-31 15:20:17 +0530

    node mongodb example part 2; addition of operations and mod of index.js; operations contain insert, update and find operations; index has nested callbacks to insert, find, update, find and delete operations; index has delete operation lsites

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -1,6 +1,8 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
+const dboper = require('./operations')
+
 const url = 'mongodb://localhost:27017/';
 const dbname = 'conFusion';
 
@@ -11,25 +13,25 @@ MongoClient.connect(url, (err,client)=>{
     console.log('connected to server');
 
     const db = client.db(dbname);
-    const collection = db.collection('dishes');
-    
-    collection.insertOne({ "name": "uthapizza", "description":"test"},(err, result)=>{
-        assert.equal(err, null);
-        console.log('After Insert: \n');
-        console.log(result.ops);
 
-        collection.find({}).toArray((err,docs)=>{
-            assert.equal(err,null);
+    dboper.insertDocument(db, {name:"vadonut", description: 'test'}, 'dishes', (result)=>{
 
-            console.log('Found: \n');
-            console.log(docs);
-            
-            db.dropCollection('dishes', (err, result)=>{
-                assert.equal(err, null);
-                client.close();
-            })
+        console.log('insert document:\n', result.ops)
 
-        })
-    })
+        dboper.findDocuments(db, 'dishes', (docs)=>{
+            console.log('Found Documents:\n', docs);
 
-})
+            dboper.updateDocument(db, { name: 'vadonut' }, { description: 'Updated test' }, 'dishes',(result)=>{
+                console.log('Updated document:\n', result)
+
+                dboper.findDocuments(db, 'dishes', (docs) => {
+                    console.log('Found Documents:\n', docs);
+                
+                    db.dropCollection('dishes', (result)=>{
+                        console.log('Dropped collection: '+ result);
+                    });
+                });
+            });
+        });
+    });
+});
