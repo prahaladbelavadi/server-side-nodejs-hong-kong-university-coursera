82b237f30d8e34c18a6c56433c0c48d06950192d
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-01-02 00:56:08 +0530

    shows error: slash in host identifier; doing mongoosecourse1

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -2,7 +2,7 @@ const mongoose  =  require('mongoose');
 
 const Dishes =  require('./models/dishes');
 
-const url  = ' mongodb://localhost:27017/conFusion';
+const url  = 'mongodb://localhost:27017/conFusion';
 
 const connect  =  mongoose.connect(url);
 
