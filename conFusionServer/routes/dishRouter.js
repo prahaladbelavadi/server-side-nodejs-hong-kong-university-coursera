41c3ecc7369d5001dff6f49dfbc0048e5feb215e
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-01-02 10:58:33 +0530

    import mongoose and models from node-mongoose

--- conFusionServer/routes/dishRouter.js
+++ conFusionServer/routes/dishRouter.js
@@ -1,28 +1,55 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Dishes =  require('../models/dishes');
 
 const dishRouter = express.Router();
 
 dishRouter.use(bodyParser.json());
 
 dishRouter.route('/')
-    .all((req, res, next) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        next();
-    })
+
     .get((req, res, next) => {
-        res.end('Will send all the dishes to you!');
+        Dishes.find({})
+        
+        .then((dish)=>{
+
+            res.statusCode = 200;
+            res.setHeader('Content-Type','applicstion/json');
+            res.json(dishes);
+        
+        },(err)=>next(err))
+        .catch((err)=>next(err));
     })
     .post((req, res, next) => {
-        res.end('Will add the dish: ' + req.body.name + ' with details: ' + req.body.description);
+        Dishes.create(req.body)
+        
+        .then((dish=>{
+
+            console.log('Dish Created:'+dish)
+
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'applicstion/json');
+            res.json(dish) ;
+            
+        }, (err) => next(err))
+            .catch((err) => next(err))
+
+        )
     })
     .put((req, res, next) => {
         res.statusCode = 403;
         res.end('PUT operation not supported on /dishes');
     })
     .delete((req, res, next) => {
-        res.end('Deleting all dishes');
+          Dishes.remove({})
+          .then((resp)=>{
+              res.statusCode = 200;
+              res.setHeader('Content-Type', 'applicstion/json');
+              res.json(resp);
+          }, (err) => next(err))
+              .catch((err) => next(err))
     });
 
 
