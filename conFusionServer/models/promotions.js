06b4b734845187ac3cc531ceaf173575280c4ae5
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-01-07 01:14:34 +0530

    begin assignment 2; add promotions schema

---
+++ conFusionServer/models/promotions.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const  promotionSchema = new Schema({
+    name:{
+        type: string,
+        required: true
+    },
+    image:{
+        type: String,
+        required: true
+    },
+    label:{
+        type: String,
+        default: ""
+    },
+    price:{
+    type:String    
+    },
+    description:{
+        type:String
+    }
+
+});
+
+var Promotions =  mongoose.model('promotion', promotionSchema);
+
+module.exports = Promotions;
