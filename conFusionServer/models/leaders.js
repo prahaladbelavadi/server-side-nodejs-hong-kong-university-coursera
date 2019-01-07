3558b348d85568a40b4e57a2a6432c1776653c63
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-01-07 15:58:04 +0530

    add schema

---
+++ conFusionServer/models/leaders.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const leaderSchema =new Schema({
+    name:{
+        type:String,
+        required: true,
+        unique:true
+    } ,
+    image:{
+        type: String,
+        default: ""
+    }, 
+    designation: {
+        type:String,
+        default: ""
+    },
+    abbr:{
+        type: String,
+    },
+     featured:{
+        default:false
+    }, 
+     description:{
+        type:String,
+        unique:true
+    }
+});
+var Leaders =  mongoose.model('leaders', leaderSchema)
