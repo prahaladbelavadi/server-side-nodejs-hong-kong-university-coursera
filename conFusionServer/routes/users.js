3570eec87eb600d21fe1c90353deaba62c0dc3fb
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-01-11 13:42:44 +0530

    shave off code; add login logic using passport.authenticate; I still think that the file needs to be imported to call passport.authenticate, but wewll find out

--- conFusionServer/routes/users.js
+++ conFusionServer/routes/users.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var User = require('../models/user');
-var passport = require('passport')
+var passport = require('passport');
+
 var router = express.Router();
 router.use(bodyParser.json()); 
 
@@ -30,43 +31,10 @@ req.body.password, (err,user)=>{
 })
 });
 
-router.post('/login', (req,res,next)=>{
-  if (!req.session.user) {
-    var authHeader = req.headers.authorization;
-    if (!authHeader) {
-      var err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      next(err);
-      return;
-    }
-    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-    var username = auth[0];
-    var password = auth[1];
-
-    User.findOne({username:username})
-    .then((user)=>{
-      if (user ===null){
-        var err = new Error('User'+ username + 'does not exist');
-        err.status = 403;
-        return next(err);
-      }else if (user.password !== password){
-        var err = new Error('Your password is incorrect!');
-        err.status = 403;
-        return next(err);
-      } 
-      else if (user.username === username && user.password === password) {
-        req.session.user = 'authenticated ';
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('You are authenticated!')
-        }
-    }).catch((err)=>next(err));
-  }else{
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('You are already authenticated!')
-  }
+router.post('/login', passport.authenticate('local'), (req,res)=> {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.json({ success: true, status: 'You are succcessfully Logged in' });
 });
 
 router.get('/logout',(req,res)=>{
