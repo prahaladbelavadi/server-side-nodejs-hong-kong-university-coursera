023c1736a8ffad70015d96c50a879d5a0754aa53
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-01-11 13:25:00 +0530

    add passport and remove code to do auth on our own; now signup process is handled by passport

--- conFusionServer/routes/users.js
+++ conFusionServer/routes/users.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var User = require('../models/user');
-
+var passport = require('passport')
 var router = express.Router();
 router.use(bodyParser.json()); 
 
@@ -14,25 +14,20 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/signup',function(req,res, next){
-User.findOne({username:req.body.username})
-.then((user)=>{
-  if (user !=null){
-    var err = new Error('User '+req.body.username + ' already exists!' );
-    err.status = 403;
-    next(err);
+User.register(new User({username:req.body.username}),
+req.body.password, (err,user)=>{
+  if (err){
+    res.statusCode = 500;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({err:err});
   }else{
-    return User.create({
-      username: req.body.username,
-      password: req.body.password
-    })
+    passport.authenticate('local')(req,res,()=>{
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.json({success:true, status: 'Registration Successful'});
+    });
   }
 })
-.then((user)=>{
-  res.statusCode = 200;
-  res.setHeader('Content-Type','application/json');
-  res.json({status:'Registration Successful', user: user});
-}, (err)=>next(err))
-.catch((err)=>next(err))
 });
 
 router.post('/login', (req,res,next)=>{
