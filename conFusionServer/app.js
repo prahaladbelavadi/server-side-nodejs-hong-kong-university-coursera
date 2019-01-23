a294e71b4ca10a60b15a7776601df30e35eae452
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-01-23 15:03:59 +0530

    restore state odf dishouter and www to sync: fdb789b54d3377b04e6d965259f38fcad3508988; readme has instructinos to create new keys but thats okay

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -23,15 +23,6 @@ const Dishes = require('./models/dishes');
 
 var app = express();
 
-// Secure traffic only
-app.all('*', (req, res, next) => {
-  if (req.secure) {
-    return next;
-  }
-  else {
-    res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
-  }
-});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
