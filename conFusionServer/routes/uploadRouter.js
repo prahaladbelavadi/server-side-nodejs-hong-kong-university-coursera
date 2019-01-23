ee21edff4d9a3d75793b9c840181c96ff640ee00
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-01-23 16:21:46 +0530

    fix file upload;

--- conFusionServer/routes/uploadRouter.js
+++ conFusionServer/routes/uploadRouter.js
@@ -1,25 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-var authenticate = require('../authenticate');
+const authenticate = require('../authenticate');
 const multer = require('multer');
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb)=>{
+    destination: (req, file, cb) => {
         cb(null, 'public/images');
     },
-    filename:(req,file,cb)=>{
+
+    filename: (req, file, cb) => {
         cb(null, file.originalname)
     }
 });
 
-const imageFileFilter = (req,file,cb) => {
-    if(!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)){
-        return cb(new Error('You can only upload image files!'))
+const imageFileFilter = (req, file, cb) => {
+    if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+        return cb(new Error('You can upload only image files!'), false);
     }
     cb(null, true);
 };
 
-const upload  = multer({storage: storage, fileFilter: imageFileFilter });
+const upload = multer({ storage: storage, fileFilter: imageFileFilter });
 
 const uploadRouter = express.Router();
 
@@ -30,19 +31,18 @@ uploadRouter.route('/')
         res.statusCode = 403;
         res.end('GET operation not supported on /imageUpload');
     })
+    .post(authenticate.verifyUser, authenticate.verifyAdminUser, upload.single('imageFile'), (req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(req.file);
+    })
     .put(authenticate.verifyUser, authenticate.verifyAdminUser, (req, res, next) => {
         res.statusCode = 403;
         res.end('PUT operation not supported on /imageUpload');
     })
     .delete(authenticate.verifyUser, authenticate.verifyAdminUser, (req, res, next) => {
         res.statusCode = 403;
-        res.end('Delete operation not supported on /imageUpload');
-    })
-    .post(authenticate.verifyUser, authenticate.verifyAdminUser, (req, res) => {
-       upload.single('imageFile'),(req,res)=>{
-           res.statusCode = 200;
-           res.setHeader('Content-Type','application/json');
-           res.json(req.file);
-       }
-    })
+        res.end('DELETE operation not supported on /imageUpload');
+    });
+
 module.exports = uploadRouter;
