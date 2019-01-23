436b9a4fd6927abb14dc6d1949ca4677b170a8e3
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-01-23 15:29:59 +0530

    HTTPS workiing; redirecting working; that next function was not called;

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -27,7 +27,7 @@ var app = express();
 // Secure traffic only
 app.all('*', (req, res, next) => {
   if (req.secure) {
-    return next;
+    return next();
   }
   else {
     res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
