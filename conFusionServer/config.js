a472c8be8d280462a4e33922e4fb955a7cc3e9ea
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-01-25 16:22:09 +0530

    Passport facebook authh

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook':{
+        clientId: '944951945709173',
+        clientSecret: '1b4314c8c42dc9c877ad69a3949d4193'
+    }
 }
